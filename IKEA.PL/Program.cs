274de7b6cda8e8d4f1abd2fb3a80fa6ddf76dbe1
Fatoma <Fatoma@gmail.com>
2025-09-15 23:12:39 +0300

    first change

--- IKEA.PL/Program.cs
+++ IKEA.PL/Program.cs
@@ -11,6 +11,7 @@ namespace IKEA.PL
         {
             var builder = WebApplication.CreateBuilder(args);
 
+
             // Add services to the container.
             builder.Services.AddControllersWithViews();
             #region Configure
